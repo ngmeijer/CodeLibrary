9984949053973c59c8fbe004e4678e6f4cb6ff38
ngmeijer <meijernils9@gmail.com>
2020-07-06 09:14:54 +0200

    Uploaded FirstPersonController

---
+++ CodeAssets/FirstPersonController/PlayerController.cs
@@ -0,0 +1,178 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerSettings))]
+[RequireComponent(typeof(PlayerAnimations))]
+public class PlayerController : MonoBehaviour
+{
+    #region Variables
+
+    //------------------------------------------------------------------//
+    [Header("Player components")]
+
+    private CharacterController charController = null;
+    private Rigidbody rb = null;
+    private SelfDestruct selfDestruct = null;
+
+    [SerializeField] private Camera playerCamera = null;
+
+    private PlayerSettings playerSettings = null;
+    private PlayerAnimations playerAnims = null;
+
+    [SerializeField] private Transform groundCheck = null;
+    private float groundDistance = 0.4f;
+    [SerializeField] private LayerMask groundMask;
+
+    private Vector3 velocity;
+    private float gravity = -9.81f;
+    private bool isGrounded = true;
+    //------------------------------------------------------------------//
+
+    //Change this to a method.
+    [HideInInspector] public int nextObjective = 0;
+    //------------------------------------------------------------------//
+
+    #endregion
+
+    #region Default methods
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerSettings = GetComponent<PlayerSettings>();
+        playerAnims = GetComponent<PlayerAnimations>();
+        selfDestruct = GetComponent<SelfDestruct>();
+        Cursor.visible = false;
+    }
+
+    private void Start()
+    {
+        nullChecks();
+    }
+
+    //Wrote this method to satisfy part of the "Excellent" criteria in the rubric.
+    private void nullChecks()
+    {
+        Debug.Assert(rb != null, "The player's RigidBody cannot be found.");
+        Debug.Assert(groundCheck != null, "The player's GroundCheck child-object is null. This means the player won't know if he's standing on the ground or not, so jumping is disabled.");
+        Debug.Assert(playerCamera != null, "The player's Camera is null. You shouldn't be able to see shit.");
+        Debug.Assert(playerSettings != null, "The PlayerSettings script is null. Check the GetComponent, or serialize it to show in the inspector and drag it in.");
+        Debug.Assert(playerAnims != null, "The PlayerAnims script is null. Check the GetComponent, or serialize it to show in the inspector and drag it in.");
+        Debug.Assert(selfDestruct != null, "The SelfDestrect script component is not attached to the Player GameObject.");
+    }
+
+    private void Update()
+    {
+        crouchPlayer();
+        rotatePlayer();
+    }
+
+    private void FixedUpdate()
+    {
+        movePlayer();
+        jumpPlayer();
+    }
+
+    #endregion
+
+    #region Movement related methods
+
+    private void movePlayer()
+    {
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float verticalMove = Input.GetAxis("Vertical");
+
+        Vector3 movementVector = new Vector3(horizontalMove, 0.0f, verticalMove) * playerSettings.moveSpeed * Time.deltaTime;
+        Vector3 newPosition = rb.position + rb.transform.TransformDirection(movementVector);
+
+        rb.MovePosition(newPosition);
+
+        //Movement for characterController (ASK AT ASSESSMENT WHAT'S BEST FOR FPS GAMES!)
+        //Vector3 movementVector = transform.right * horizontalMove + transform.forward * verticalMove;
+        //charController.Move(movementVector * playerSettings.moveSpeed * Time.deltaTime);
+
+        //if (isGrounded && velocity.y < 0)
+        //{
+        //    velocity.y = playerSettings.downForce;
+        //}
+
+
+        if (Input.GetKey(playerSettings.sprintKey))
+        {
+            playerSettings.moveSpeed = playerSettings.sprintSpeed;
+        }
+        else
+        {
+            playerSettings.moveSpeed = playerSettings.walkSpeed;
+        }
+
+
+        if (verticalMove > 0)
+        {
+            playerAnims.handleRunAnimation();
+        }
+        else
+        {
+            playerAnims.handleIdleAnimation();
+        }
+    }
+
+    private void jumpPlayer()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded)
+        {
+            if (Input.GetKey(playerSettings.jumpKey))
+            {
+                Vector3 jumpVector = new Vector3(0.0f, playerSettings.jumpForce, 0.0f) * Time.deltaTime;
+                rb.AddForce(jumpVector);
+            }
+        }
+
+        //CharacterController jumping
+        //velocity.y += gravity * Time.deltaTime;
+
+        ////charController.Move(velocity * Time.deltaTime);
+
+        //if (Input.GetKey(playerSettings.jumpKey) && isGrounded)
+        //{
+        //    velocity.y = Mathf.Sqrt(playerSettings.jumpForce * playerSettings.downForce * gravity * Time.deltaTime);
+        //}
+    }
+
+    private void crouchPlayer()
+    {
+        if (Input.GetKey(playerSettings.crouchKey))
+        {
+            playerCamera.transform.localPosition = playerSettings.camCrouchPosition;
+            playerAnims.handleCrouchAnimation();
+            //charController.height = 0.3f;
+        }
+        else
+        {
+            playerCamera.transform.localPosition = playerSettings.camNormalPosition;
+            //charController.height = 0.8f;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Death"))
+        {
+            selfDestruct.resetLevel = true;
+        }
+    }
+
+    #endregion
+
+    #region Camera related Methods
+
+    private void rotatePlayer()
+    {
+        float mouseHorizontal = Input.GetAxis("Mouse X") * playerSettings.rotateSpeed * Time.deltaTime;
+
+        transform.Rotate(Vector3.up * mouseHorizontal);
+    }
+
+    #endregion
+}
---
+++ CodeAssets/FirstPersonController/PlayerSettings.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSettings : MonoBehaviour
+{
+    #region Variables
+
+    //------------------------------------------------------------------//
+    [Header("Input keys")]
+    [SerializeField] public KeyCode crouchKey = KeyCode.LeftControl;
+    [SerializeField] public KeyCode jumpKey = KeyCode.Space;
+    [SerializeField] public KeyCode sprintKey = KeyCode.LeftShift;
+    [SerializeField] public KeyCode reverseViewKey = KeyCode.Q;
+    [SerializeField] public KeyCode dashKey = KeyCode.CapsLock;
+    [SerializeField] public KeyCode activateInventoryKey = KeyCode.Tab;
+    [SerializeField] public KeyCode explosionAbilityKey = KeyCode.E;
+
+    //------------------------------------------------------------------//
+
+    //Before I remove all [SerializeFields], is there any other way of accessiblity instead of public? Thought protected but it only allows it for parents..
+
+    //------------------------------------------------------------------//
+    [Header("Movement values")]
+    [SerializeField] public float walkSpeed = 4f;
+    [SerializeField] public float sprintSpeed = 8f;
+    [SerializeField] public float speedIncrease = 1;
+
+    public float moveSpeed = 0f;
+
+    [SerializeField] public float rotateSpeed = 100f;
+
+    [SerializeField] public float jumpForce = 100f;
+
+    [SerializeField] public float boostPower = 5f;
+
+    [SerializeField] public float downForce = -1f;
+
+    [SerializeField] public float forceRadius = 1f;
+    [SerializeField] public float explosionForce = 500f;
+    [SerializeField] public float maxExplosionForce = 1500f;
+    [SerializeField] public float maxTimeAlive;
+
+    [SerializeField] public Vector3 camNormalPosition = new Vector3(0f, 1f, 0f);
+    [SerializeField] public Vector3 camCrouchPosition = new Vector3(0f, -0.8f, 0f);
+    [SerializeField] public Quaternion reverseViewRotation = new Quaternion(0f, 180f, 0f, 0f);
+    [SerializeField] public Quaternion normalViewRotation = new Quaternion(0f, 0f, 0f, 0f);
+    //------------------------------------------------------------------//
+
+    #endregion
+}
